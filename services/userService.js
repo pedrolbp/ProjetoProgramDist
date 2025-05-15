0d84a0981c114046087c3ff8efd29b0f8e5f0cfc
pedrolbp <pedrolbarriola@gmail.com>
2025-05-15 17:28:21 -0300

    add service

---
+++ services/userService.js
@@ -0,0 +1,56 @@
+import User from '../models/User.js';
+
+const registerUserService = async (name, password, emailAsLogin) => {
+    const existingUser = await User.findOne({ login: emailAsLogin });
+    if (existingUser) {
+        throw new Error('User with this login already exists');
+    }
+
+    const newUser = new User({
+        name,
+        login: emailAsLogin,
+        password: password,
+        emailConfirmed: false
+    });
+
+    await newUser.save();
+    const userObject = newUser.toObject();
+    delete userObject.password;
+    return userObject;
+};
+
+const loginUserService = async (emailAsLogin, password) => {
+    const user = await User.findOne({ login: emailAsLogin });
+
+    if (!user) {
+        throw new Error('Invalid credentials');
+    }
+
+    if (user.password !== password) {
+        throw new Error('Invalid credentials');
+    }
+
+    if (!user.emailConfirmed) {
+        throw new Error('User not confirmed yet. Please confirm your email.');
+    }
+    
+    const userObject = user.toObject();
+    delete userObject.password;
+    return userObject;
+};
+
+const forgotPasswordService = async (emailAsLogin) => {
+    const user = await User.findOne({ login: emailAsLogin });
+
+    if (!user) {
+        throw new Error('User with this login not found');
+    }
+
+    return { message: `If a user with login ${emailAsLogin} exists, further instructions for password reset would be processed here.` };
+};
+
+export {
+    registerUserService,
+    loginUserService,
+    forgotPasswordService
+};
